c3006457f25007946261203ad3c6b4e34e9f1e9f
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-09-18 12:25:43 +0100

    Added new column, StartDate, with data.

---
+++ EmploymentManagement.Migrations/Migrations/3_AddStartDateColumn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FluentMigrator;
+
+namespace EmploymentManagement.Migrations.Migrations
+{
+    [Migration(201809181203)]
+    public class AddStartDateColumn : Migration
+    {
+        private const string EmployeesTable = "Employees";
+        private const string StartDateColumn = "StartDate";
+
+        public override void Up()
+        {
+            Alter.Table(EmployeesTable)
+                .AddColumn(StartDateColumn)
+                .AsDate()
+                .SetExistingRowsTo(new DateTime(2010, 1, 1).Date)
+                .NotNullable();
+
+            Update.Table(EmployeesTable)
+                .Set(new { StartDate = new DateTime(2016, 12, 3).Date })
+                .Where(new { EmployeeId = 1 });
+
+            Update.Table(EmployeesTable)
+                .Set(new { StartDate = new DateTime(2018, 3, 4).Date })
+                .Where(new { EmployeeId = 2 });
+        }
+
+        public override void Down()
+        {
+            Delete.Column(StartDateColumn).FromTable(EmployeesTable);          
+        }
+    }
+}
